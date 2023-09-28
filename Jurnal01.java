94133e59be08ff3351884af95fecb7a17449be4c
dnraa26 <ariani.ixk@gmail.com>
2023-09-28 15:18:35 +0700

    Kumpul Jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String Nama = "Danira Ariani Ningtyas";
+        String NIM = "607062300037";
+        String Kelas = "D3IF-47-01";
+
+        System.out.println("Nama: " + Nama);
+        System.out.println("NIM: " + NIM);
+        System.out.println("Kelas: " + Kelas);
+    }
+}
